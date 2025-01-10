5d70082f240d6e9d62e23bd95444a22010fc31e6
DIA <5084kkm@naver.com>
2025-01-11 02:16:10 +0900

    add gameManager

---
+++ Assets/01.Script/GameManager/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public List<int> armItemList = new List<int>();
+    public List<Item> ItemList = new List<Item>(); // index 0 = NULL
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //아이템 리스트 가져오기?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
