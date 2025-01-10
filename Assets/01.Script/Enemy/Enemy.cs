997b8705b8287a05fdb7254deb2fd1c0456fa98a
DIA <5084kkm@naver.com>
2025-01-10 19:53:35 +0900

    Add ObjectPool, enemy spawner

--- Assets/01.Script/Enemy/Enemy.cs
+++ Assets/01.Script/Enemy/Enemy.cs
@@ -1,24 +1,46 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.UIElements;
 
-public class Enemy : MonoBehaviour
-{
-
-
+public class Enemy : PoolAble
+{ 
     [SerializeField]
     private int hp = 1;
-    private int speed = 1;
+    [SerializeField]
+    private float speed = 0.1f;
+    [SerializeField]
     private int damage = 1;
     private GameObject player;
 
-    void Start()
+    private void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed);
+    }
+
+    public void Damage(int damage)
+    {
+        if(hp <= 0)
+        {
+            Destroy();
+        }
+    }
+    public void Destroy()
+    {
+        ReleaseObject();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject == player)
+        {
+            //todo
+            //player 데미지 주기
+        }
     }
 }
