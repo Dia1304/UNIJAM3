cbc9f5c9ed18abeef02ceeeeafd646121da6133e
보리차 <bumgyuseo101@gmail.com>
2025-01-11 20:14:26 +0900

    more menus, weapon sprites

--- Assets/01.Script/UI/MainMenu.cs
+++ Assets/01.Script/UI/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void EnterGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(2);
     }
     public void EnterSettings()
     {
