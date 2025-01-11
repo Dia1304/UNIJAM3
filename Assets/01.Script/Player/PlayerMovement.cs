4e5c87e049c9c6e48fa3b8e5ed73c9a2d0b61cf7
보리차 <bumgyuseo101@gmail.com>
2025-01-11 22:14:58 +0900

    player walk animation

--- Assets/01.Script/Player/PlayerMovement.cs
+++ Assets/01.Script/Player/PlayerMovement.cs
@@ -2,9 +2,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Animator anim;
+    private bool isFacingRight;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         transform.position += (Vector3)PlayerController.instance.GetDirection() * PlayerController.instance.Stat.moveSpeed * Time.unscaledDeltaTime;
+        if (PlayerController.instance.GetDirection() != Vector2.zero )
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+
+        if(!isFacingRight && PlayerController.instance.GetDirection().x > 0)
+        {
+            Flip();
+        }
+        else if(isFacingRight && PlayerController.instance.GetDirection().x < 0)
+        {
+            Flip();
+        }
+    }
+
+    public void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
     }
 }
